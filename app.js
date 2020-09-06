5769daa94af8a0281abda06e4cd3a83f4f8c5952
AndyCuba <70431231+AndyCuba@users.noreply.github.com>
2020-09-07 02:34:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -96,12 +96,109 @@
 // alert( sumInput ())
 
 
-let user = {
-    name: "John",
-    age: 30
-  };
+// let user = {
+//     name: "John",
+//     age: 30
+//   };
   
-  let key = prompt("Что вы хотите узнать о пользователе?", "name");
+//   let key = prompt("Что вы хотите узнать о пользователе?", "name");
   
-  // доступ к свойству через переменную
-  alert( user[key] ); // John (если ввели "name")
+//   // доступ к свойству через переменную
+//   alert( user[key] ); // John (если ввели "name")
+
+
+//   let fruit = prompt("Какой фрукт купить?", "apple");
+
+//   let bag = {
+//     [fruit]: 5, // имя свойства будет взято из переменной fruit
+//   };
+  
+//   alert( bag.a ); // 5, если fruit="apple"
+
+
+// Objects
+
+// user = {}
+// user.name = 'John'
+// user.surname = 'Smith'
+// user.name = 'Pete'
+// delete user.name
+// console.log(user)
+
+//1
+
+// function isEmpty(obj) {
+//     for (let key in obj) {
+//         return false
+//     }
+//     return true
+// }
+
+// let schedule = {};
+
+// alert( isEmpty(schedule) )
+
+// schedule["8:30"] = "get up";
+
+// alert( isEmpty(schedule) );
+
+
+//2
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+//   }
+//1111111111111111
+// function salary ( obj ) {
+//      let sum = 0
+//      for (let key in obj) {
+//         return (sum += obj[key]);
+//     }
+// }
+// salary ( salaries )
+// alert( sum )
+
+
+
+
+// let menu = {
+//     width: 200,
+//     height: 300,
+//     title: "My menu"
+//   };
+// multiplyNumeric ( menu )
+// function multiplyNumeric(obj) {
+//     for (let key in obj) {
+//         if ( typeof obj[key] == 'number') {
+//             obj[key] *= 2
+//         }
+//     }
+// }
+
+
+// function checkDriverAge() {
+//     let age = prompt("What is your age?");
+//     if (Number(age) < 18) {
+//         alert("Sorry, you are too yound to drive this car. Powering off");
+//     } else if (Number(age) > 18) {
+//         alert("Powering On. Enjoy the ride!");
+//     } else if (Number(age) === 18) {
+//         alert("Congratulations on your first year of driving. Enjoy the ride!");
+//     }
+// }
+
+
+
+
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+array.shift ()
+array.sort()
+array.push("Kiwi")
+array.splice(0, 1)
+array.reverse()
+console.log( array )
+
+var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+console.log( array2[1][1][0]);
