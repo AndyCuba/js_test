12db9fea7b7469b31c287dad8b96656261430da3
AndyCuba <zfesandydayona@gmail.com>
2020-09-09 15:50:55 +0300

    changes to comment

--- app.js
+++ app.js
@@ -192,13 +192,13 @@
 
 
 
-var array = ["Banana", "Apples", "Oranges", "Blueberries"];
-array.shift ()
-array.sort()
-array.push("Kiwi")
-array.splice(0, 1)
-array.reverse()
-console.log( array )
+// var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+// array.shift ()
+// array.sort()
+// array.push("Kiwi")
+// array.splice(0, 1)
+// array.reverse()
+// console.log( array )
 
-var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
-console.log( array2[1][1][0]);
+// var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// console.log( array2[1][1][0]);
